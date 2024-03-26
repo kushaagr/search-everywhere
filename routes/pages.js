d16ac57f4905c183736af91f2c2b5d8982994009
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2024-03-26 20:34:05 +0530

    Add search function Fix some broken styling

--- routes/pages.js
+++ routes/pages.js
@@ -9,10 +9,13 @@ router.get("/", (req, res) => {
         res.render("index");
     } else {
         fetchResponse(req).then((response) => {
-            console.log(response['duckduckgo'] ?? response['duckduckgo'].slice(0, 3));  
+            console.log(response['duckduckgo'] ? response['duckduckgo'].slice(0, 3) : null); 
+            console.log(response['reddit'] ? response['reddit'].slice(0, 3) : null); 
             // console.log(response['duckduckgo']);
             // res.send(response['duckduckgo'].slice(0, 3));
             res.render("search", { searchresults: response });
+        }).catch(err => {
+            console.error(`error.response = ${err.response}`)
         });   
     }
 
