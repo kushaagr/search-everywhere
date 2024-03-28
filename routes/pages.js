2591ef49f1b0a0f271bbda539f06e5133e9840d1
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2024-03-28 05:35:05 +0530

    Update error handling code

--- routes/pages.js
+++ routes/pages.js
@@ -9,16 +9,23 @@ router.get("/", (req, res) => {
         res.render("index");
     } else {
         fetchResponse(req).then((response) => {
-            console.log(response['duckduckgo'] ? response['duckduckgo'].slice(0, 3) : null); 
-            console.log(response['reddit'] ? response['reddit'].slice(0, 3) : null); 
+            // console.log(response['duckduckgo'] ? response['duckduckgo'].slice(0, 3) : null); 
+            // console.log(response['reddit'] ? response['reddit'].slice(0, 1) : null); 
+            const now = new Date();
             // console.log(response['duckduckgo']);
             // res.send(response['duckduckgo'].slice(0, 3));
             res.render("search", { searchresults: response, searchquery: req.query.q});
+            console.log(`Data sent @ ${now.toLocaleString()}`);
+            return;
         }).catch(err => {
-            console.error(`error.response = ${err.response}`)
-        });   
+            console.error(`error.response = ${err}`)
+            // res.sendStatus(500);
+            res.status(500).end();
+            return;
+        });
     }
 
 })
+
     
 export default router;
